08f0bf59e88e130d828c7dfc7e97e9d48c829281
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-03-18 17:56:33 -0500

    menus nuevos

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,6 +1,43 @@
 export function Home(){
     return(
         <>
+            <section className="container mt-5">
+            <div className="row">
+                <div className="col-12 col-md-6">
+                    <h2 className="fuente">servicios a un clic</h2>
+                    <h2 className="text-muted">HOLA <span className="fuente">GERALDIN </span></h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum voluptate dignissimos quas impedit qui tempora voluptatibus veritatis suscipit, similique, deleniti soluta omnis, nam sed consectetur nulla eaque obcaecati ratione sunt officiis sapiente incidunt? Aliquid quidem distinctio sequi quae, hic, id nihil eius voluptatibus ab soluta perspiciatis, omnis qui mollitia amet?</p>
+                </div>
+                <div className="col-12 col-md-6">
+                <select class="form-select" aria-label="Default select example">
+                 <option selected>GRUPO FAMILIAR</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                 <option value="3">Three</option>
+                </select>
+                <div className="row">
+                    <div className="col-12 col-md-2">
+                    <i class="bi bi-person-down fs-1 fuente"></i>
+                        <div className="col-12 col-md-10">
+                            <p>Dr Natalia Blandon</p>
+
+                        </div>
+                    </div>
+
+
+                </div>
+                <di className="row">
+
+                </di>
+                <di className="row">
+
+                </di>
+                <di className="row">
+
+                </di>
+                </div>
+            </div>
+        </section>
         
         </>
     )
