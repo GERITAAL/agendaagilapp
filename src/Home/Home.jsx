2fdaca02a448c271941435f41ade667eacad9053
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-03-21 16:44:05 -0500

    anexo con js yjson

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,29 +1,17 @@
 import"./Home.css"
-import { inventario } from "../Utils/medicamentos"
+import basedatos from"../Utils/basedatos.json"
 
+console.log(basedatos[0])
 
 export function Home(){
-    // zona de manipulacion de datos (parte logica)
-    let nombrePaciente="Geraldin Bernal Mazo"
-    let sedePaciente="Suramericana"
 
-    console.log(inventario)
-
-    // a los arreglos los puedo manipular para controlar que informacion voy a presentar
-    // funcion de filtrado de datos , condicion de filtrado (pregunta si o no)
-
-    let resultado=inventario.filter(function(auxiliar){
-        return(auxiliar.nombre=="acetaminofen") // se pone condicion de filtrado
-    })
-            console.log(resultado)
-            
     return( // zona de renderizado( la parte estetica)
         <>
             <section className="container mt-5">
             <div className="row">
                 <div className="col-12 col-md-6">
                     <h2 className="fuente">servicios a un clic</h2>
-                    <h2 className="text-muted">HOLA <span className="fuente">{nombrePaciente}</span></h2>
+                    <h2 className="text-muted">HOLA <span className="fuente">{basedatos[0].nombreUsuario}</span></h2>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum voluptate dignissimos quas impedit qui tempora voluptatibus veritatis suscipit, similique, deleniti soluta omnis, nam sed consectetur nulla eaque obcaecati ratione sunt officiis sapiente incidunt? Aliquid quidem distinctio sequi quae, hic, id nihil eius voluptatibus ab soluta perspiciatis, omnis qui mollitia amet?</p>
                 </div>
                 <div className="col-12 col-md-6">
@@ -40,23 +28,66 @@ export function Home(){
                     </div>
                     
                         <div className="col-12 col-md-10 mt-5">
-                            <p>Dr Natalia Blandon</p>
+                            <p>medico de familia {basedatos[0].medicoDeFamilia}</p>
                         </div>
 
                 </div>
                 <div className="row">
+                    <div className="col-12 col-md-2 mt-5">
+                    <i class="bi bi-person fs-1 fuente mb-5"></i>
+                    </div>
                     
+                    <div className="col-12 col-md-6 mt-5">
+                        <p>{basedatos[0].grupoDeIngreso}</p>
+                    
+                    </div>
+
                 </div>
-                <di className="row">
+                <div className="row">
+                    <div className="col-12 col-md-6">
+                    <i class="bi bi-hospital fs-1 fuente"></i>
+                    </div>
 
-                </di>
-                <di className="row">
 
-                </di>
+                </div>
+                <div className="row">
+                    <div className="col-12 col-md-6">
+                        <p>{basedatos[0].ips}</p>
+                    </div>
+                </div>
                 </div>
             </div>
         </section>
+
+        <hr />
         
+        <section className="container">
+            <div className="row">
+                <div className="col-12 col-md-5">
+                    <h5>paciente{basedatos[0].nombreUsuario}, estas son tus proximas citas</h5>
+
+                </div>
+            </div>
+
+        </section>
+        <section className="container my-5">
+            <div className="row row-cols-1 row-cols-md-3">
+                {
+                    basedatos[0].citasMedicas.map(function(cita){
+                        return(
+                            <div className="col">
+                                <div className="card h-100 shadow px-2">
+                                    <h3>{cita.especialidad}</h3>
+                                    <h4>{cita.fecha}</h4>
+                                    <h4>{cita.direccion}</h4>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+
+            </div>
+        </section>
         </>
     )
 }
